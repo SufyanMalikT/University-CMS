e3bbe0dfc854e5044205931813e61e7aca637cb4
SufyanMalikT <sufyanmaliksaifi@gmail.com>
2026-06-30 20:24:09 +0500

    Optimized and completed gradebook page

--- practice/settings.py
+++ practice/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == True
+DEBUG = os.getenv('MY_DEBUG') or True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
